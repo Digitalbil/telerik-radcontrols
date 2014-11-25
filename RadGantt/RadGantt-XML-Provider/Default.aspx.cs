d215cfc187262405a73a97eebff36ba4de483911
hvalyavicharski <h.valyavicharski@gmail.com>
2014-11-25 15:33:44 +0200

    ADD: Gantt XML Provider

---
+++ RadGantt/RadGantt-XML-Provider/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+
+public partial class Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void Page_Init(object sender, EventArgs e)
+    {
+        RadGantt1.Provider = new XmlGanttProvider(Server.MapPath("~/App_Data/GanttData.xml"), false); ;
+    }
+}
---
+++ RadGantt/RadGantt-XML-Provider/README.md
@@ -0,0 +1,6 @@
+﻿# RadGantt
+
+[Demos](http://demos.telerik.com/aspnet-ajax/gantt/examples/overview/defaultcs.aspx)
+[Help](http://www.telerik.com/help/aspnet-ajax/gantt-overview.html)
+
+## RadGantt XML Provider
