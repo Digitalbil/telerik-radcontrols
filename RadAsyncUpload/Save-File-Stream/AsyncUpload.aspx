47bb9ade01ccffed51c33299e9c37815e1744a9a
hvalyavicharski <h.valyavicharski@gmail.com>
2015-02-24 14:16:37 +0200

    ADD Sample project showing how to save file from the uploaded file stream

---
+++ RadAsyncUpload/Save-File-Stream/AsyncUpload.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="AsyncUpload.aspx.cs" Inherits="AsyncUpload" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+            <Scripts>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+            </Scripts>
+        </telerik:RadScriptManager>
+        <script type="text/javascript">
+            //Put your JavaScript code here.
+        </script>
+        <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server">
+        </telerik:RadAjaxManager>
+        <div>
+
+            <telerik:RadAsyncUpload ID="RadAsyncUpload1" runat="server" />
+            <telerik:RadButton runat="server" ID="btnSubmit" Text="Submit" OnClick="btnSubmit_Click"></telerik:RadButton>
+            <asp:Label ID="lblResult" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RadAsyncUpload/Save-File-Stream/AsyncUpload.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+using System.IO;
+
+public partial class AsyncUpload : System.Web.UI.Page 
+{
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        foreach (UploadedFile file in RadAsyncUpload1.UploadedFiles)
+        {
+            string path = Server.MapPath("~/Uploads/") + file.FileName;
+            bool isUplodad = SaveFile(file.InputStream, path);
+            ShowResult(isUplodad);
+        }
+    }
+
+    public bool SaveFile(Stream stream, string fulleFileName)
+    {
+        try
+        {
+            byte[] buffer = new byte[1024];
+            using (FileStream fs = new FileStream(fulleFileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                int read;
+                while ((read = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, read);
+                }
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+        return true;
+    }
+
+    public void ShowResult(bool isUpload)
+    {
+        if (isUpload)
+        {
+            lblResult.Text = "File was uploaded successfully.";
+        }
+        else
+        {
+            lblResult.Text = "Error!. The file was not uploaded.";
+        }
+    }
+}
