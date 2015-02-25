2fea89eb9a2738430ed4a21fefee7c5670ed7406
hvalyavicharski <h.valyavicharski@gmail.com>
2015-02-25 17:08:06 +0200

    ADD Demo showing how to implement drag and drop functionality from RadDropDownTree to RadGrid

---
+++ RadDropDownList/RadGrid-Drag-and-Drop-RadDropDownTree/Default.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <telerik:RadStyleSheetManager ID="RadStyleSheetManager1" runat="server" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <telerik:RadScriptManager ID="RadScriptManager1" runat="server">
+            <Scripts>
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.Core.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQuery.js" />
+                <asp:ScriptReference Assembly="Telerik.Web.UI" Name="Telerik.Web.UI.Common.jQueryInclude.js" />
+            </Scripts>
+        </telerik:RadScriptManager>
+
+        <script type="text/javascript">
+            var gridElement;
+
+            function onDropping(sender, args) {
+                var source = args.get_sourceNode();
+                var destElement = args.get_htmlElement();
+
+
+                //The node is dropped on the grid
+                if ($(gridElement).has(destElement).length > 0) {
+                    //Pass the node value to the server
+                    $find('RadAjaxManager1').ajaxRequest(source.get_value());
+                }
+            }
+
+            function pageLoad() {
+                var grid = $find('RadGrid1');
+                gridElement = grid.get_element();
+            }
+        </script>
+
+        <telerik:RadAjaxManager ID="RadAjaxManager1" runat="server" OnAjaxRequest="RadAjaxManager1_AjaxRequest">
+            <AjaxSettings>
+                <telerik:AjaxSetting AjaxControlID="RadAjaxManager1">
+                    <UpdatedControls>
+                        <telerik:AjaxUpdatedControl ControlID="RadGrid1" />
+                    </UpdatedControls>
+                </telerik:AjaxSetting>
+            </AjaxSettings>
+        </telerik:RadAjaxManager>
+
+        <telerik:RadDropDownTree ID="RadDropDownTree1" runat="server">
+        </telerik:RadDropDownTree>
+
+        <telerik:RadGrid ID="RadGrid1" runat="server" OnNeedDataSource="RadGrid1_NeedDataSource"></telerik:RadGrid>
+    </form>
+</body>
+</html>
---
+++ RadDropDownList/RadGrid-Drag-and-Drop-RadDropDownTree/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using Telerik.Web.UI;
+
+public partial class Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            RadDropDownTree1.DataFieldID = "ID";
+            RadDropDownTree1.DataFieldParentID = "ParentID";
+            RadDropDownTree1.DataValueField = "Value";
+            RadDropDownTree1.DataTextField = "Text";
+            RadDropDownTree1.DataSource = DropDownTreeData();
+            RadDropDownTree1.DataBind();
+            RadDropDownTree1.EmbeddedTree.EnableDragAndDrop = true;
+            RadDropDownTree1.EmbeddedTree.OnClientNodeDropping = "onDropping";
+
+            Session["GridData"] = GridData();
+        }
+    }
+
+    public DataTable DropDownTreeData()
+    {
+        DataTable table = new DataTable();
+        table.Columns.Add("ID");
+        table.Columns.Add("ParentID");
+        table.Columns.Add("Value");
+        table.Columns.Add("Text");
+
+        table.Rows.Add(new String[] { "1", null, "World_Continents", "World Continents" });
+        table.Rows.Add(new String[] { "2", null, "World_Oceans", "World Oceans" });
+
+        table.Rows.Add(new String[] { "3", "1", "Asia", "Asia" });
+        table.Rows.Add(new String[] { "4", "1", "Africa", "Africa" });
+        table.Rows.Add(new String[] { "5", "1", "Australia", "Australia" });
+        table.Rows.Add(new String[] { "6", "1", "Europe", "Europe" });
+        table.Rows.Add(new String[] { "7", "1", "North_America", "North America" });
+        table.Rows.Add(new String[] { "8", "1", "South_America", "South America" });
+
+        table.Rows.Add(new String[] { "9", "2", "Arctic_Ocean", "Arctic Ocean" });
+        table.Rows.Add(new String[] { "10", "2", "Atlantic_Ocean", "Atlantic Ocean" });
+        table.Rows.Add(new String[] { "11", "2", "Indian_Ocean", "Indian Ocean" });
+        table.Rows.Add(new String[] { "12", "2", "Pacific_Ocean", "Pacific Ocean" });
+        table.Rows.Add(new String[] { "13", "2", "South_Ocean", "SouthOcean" });
+
+        return table;
+    }
+
+    public DataTable GridData() 
+    { 
+        DataTable dt = new DataTable();
+        dt.Columns.Add("Country", typeof(string));
+
+        dt.Rows.Add("USA");
+
+        return dt;
+    }
+
+    protected void RadGrid1_NeedDataSource(object sender, GridNeedDataSourceEventArgs e)
+    {
+        RadGrid1.DataSource = (DataTable)Session["GridData"];
+    }
+    protected void RadAjaxManager1_AjaxRequest(object sender, AjaxRequestEventArgs e)
+    {
+        UpdateDataSource(e.Argument);
+    }
+
+    private void UpdateDataSource(string nodeValue)
+    {
+       ((DataTable)Session["GridData"]).Rows.Add(nodeValue);
+       RadGrid1.Rebind();
+    }
+}
